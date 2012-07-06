1fa8d0ee25f29287d891ec576917d772bb4256d5
cforce <cforce@gmx.de>
2012-07-06 15:01:29 +0200

    fix bug withj case sensitive controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:id/templatesg/:action', :controller => 'Templatesg'
-  map.connect 'templatesg/:action', :controller => 'Templatesg'
-  map.connect 'template/:action', :controller => 'Templates'
-  map.connect 'projects/:id/settings/template', :controller => 'Templates'
+  map.connect 'projects/:id/templatesg/:action', :controller => 'templatesg'
+  map.connect 'templatesg/:action', :controller => 'templatesg'
+  map.connect 'template/:action', :controller => 'templates'
+  map.connect 'projects/:id/settings/template', :controller => 'templates'
   #map.connect 'projects/:id/settings/:action', :controller => 'Templates'
 end
